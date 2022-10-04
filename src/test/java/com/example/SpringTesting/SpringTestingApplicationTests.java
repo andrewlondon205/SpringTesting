ad140fa788e50d39cb6a21349c3291eca028f1dd
AlejandriaRamai <vyasadev931@gmail.com>
2022-10-04 17:36:41 +0200

    Initial commit

---
+++ src/test/java/com/example/SpringTesting/SpringTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
