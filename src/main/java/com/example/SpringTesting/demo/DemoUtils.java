2eb4328ad2803147c6960eaf03b7641bcdfb381a
AlejandriaRamai <vyasadev931@gmail.com>
2022-10-11 16:12:11 +0200

    new implementations

--- src/main/java/com/example/SpringTesting/demo/DemoUtils.java
+++ src/main/java/com/example/SpringTesting/demo/DemoUtils.java
@@ -1,6 +1,14 @@
 package com.example.SpringTesting.demo;
 
 public class DemoUtils {
+    public int add(int a, int b) {return a+b; }
+
+    public Object checkNull (Object obj) {
+        if (obj != null ) {
+            return obj;
+        }
+        return null;
+    }
 
 
-}
+ }
--- src/test/java/com/example/SpringTesting/DemoUtilsTest.java
+++ src/test/java/com/example/SpringTesting/DemoUtilsTest.java
@@ -1,14 +1,28 @@
 package com.example.SpringTesting;
 
+import com.example.SpringTesting.demo.DemoUtils;
+import static  org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 public class DemoUtilsTest {
 
 	@Test
-	public void testEqualsAndNotEquals () {
+	public void testEquals () {
 
+		DemoUtils demoUtils = new DemoUtils();
+
+		assertEquals(6,demoUtils.add(2,4), "2+4 must be 6");
+		assertNotEquals(8,demoUtils.add(1,9), "2+4 must be 6");
 	}
+
+	@Test
+	public void nullAndNotNullTest () {
+		DemoUtils demoUtils = new DemoUtils();
+		String str1 = null;
+		String str2 = "helloworld";
+		assertNull(demoUtils.checkNull(str1),"Object should be null");
+		assertNotNull(demoUtils.checkNull(str2),"Object should not be null");
+	}
+
 
 }
